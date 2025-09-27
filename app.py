f5aaec0d3d5e6cc9a2fa2fa36c4b01c8ce4fd554
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-09-27 11:59:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from models.user import Base
 from routes.user_register import user_register_bp
-from routes.user_login import user_login_bp
+from routes.login import login_bp
 from database.connection import sa_engine
 from dotenv import load_dotenv
 from services.auth import _get_secret_key
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 # Registrar blueprints no app Flask
 app.register_blueprint(user_register_bp)
-app.register_blueprint(user_login_bp)
+app.register_blueprint(login_bp)
 
 # Criar tabelas no banco
 Base.metadata.create_all(bind=sa_engine)
